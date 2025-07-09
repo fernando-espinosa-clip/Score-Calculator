c4a0c6a31c90d59099b796a5aeba81fecf031242
fernando-espinosa-clip <fernando.espinosa@payclip.com>
2025-07-08 19:10:56 -0600

    Refactor `getCookie` function: comment out unused cookie parsing logic in `OptimizationCalculator` component.

--- src/components/OptimizationCalculator.tsx
+++ src/components/OptimizationCalculator.tsx
@@ -34,14 +34,14 @@ const OptimizationCalculator: React.FC = () => {
   // Función para obtener el valor de una cookie por su nombre
   const getCookie = (name: string): string | null => {
     return document.cookie;
-    const cookies = document.cookie.split(';');
+    /*const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
       if (cookie.startsWith(name + '=')) {
         return cookie.substring(name.length + 1);
       }
     }
-    return null;
+    return null;*/
   };
 
   // Obtener el valor de la cookie al cargar el componente
